404b3a66575121b1bb90c925c0a4c25a1024f4fb
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2022-02-08 19:13:04 +0300

    fix prettier

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,12 @@
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
-program.description('Compares two configuration files and shows a difference.').version('0.0.1', '-V, --version', 'output the version number');
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number');
 
 program.parse();
 
-const options = program.opts();
+/* const options = program.opts();
 if (options.version) console.log('0.0.1');
+ */
