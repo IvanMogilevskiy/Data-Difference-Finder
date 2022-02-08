8844efb278fec55c8c74bc952b1bc24777d051a8
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2022-02-08 14:55:42 +0300

    bin/

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+
+program.description('Compares two configuration files and shows a difference.').version('0.0.1', '-V, --version', 'output the version number');
+
+program.parse();
+
+const options = program.opts();
+if (options.version) console.log('0.0.1');
