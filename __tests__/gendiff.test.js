b1697ec4838777fefd44b800be43a19d9b162aef
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2022-02-21 14:31:45 +0300

    add tests for yml and rename fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,13 +3,20 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import gendiff from '../src/diff.js';
 
-test('gendiff', () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const expectedOutput = readFile('expectedOutput.txt');
 
-  const expectedOutput = readFile('expectedOutput.txt');
+test('gendiff json', () => {
+  expect(gendiff('filepath1.json', 'filepath2.json')).toEqual(expectedOutput);
+});
 
-  expect(gendiff('file1.json', 'file2.json')).toEqual(expectedOutput);
+test('gendiff yaml', () => {
+  expect(gendiff('filepath1.yml', 'filepath2.yml')).toEqual(expectedOutput);
+});
+
+test('gendiff json & yaml', () => {
+  expect(gendiff('filepath1.json', 'filepath2.yml')).toEqual(expectedOutput);
 });
