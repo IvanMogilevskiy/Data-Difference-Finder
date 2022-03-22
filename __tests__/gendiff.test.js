628c4b45600a7feb4f7cfbb1db04622bade38601
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2022-03-22 19:02:42 +0300

    add test for stylish

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,9 +7,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const expectedOutput = readFile('expectedOutput.txt');
+//const expectedOutput = readFile('expectedOutput.txt');
+const expectedOutputNested = readFile('expectedOutputNested.txt');
 
-test('gendiff json', () => {
+/* test('gendiff json', () => {
   expect(gendiff('filepath1.json', 'filepath2.json')).toEqual(expectedOutput);
 });
 
@@ -19,4 +20,17 @@ test('gendiff yaml', () => {
 
 test('gendiff json & yaml', () => {
   expect(gendiff('filepath1.json', 'filepath2.yml')).toEqual(expectedOutput);
+});
+ */
+
+test('gendiff json', () => {
+  expect(gendiff('file1.json', 'file2.json')).toEqual(expectedOutputNested);
+});
+
+test('gendiff yaml', () => {
+  expect(gendiff('file1.yml', 'file2.yml')).toEqual(expectedOutputNested);
+});
+
+test('gendiff json & yaml', () => {
+  expect(gendiff('file1.json', 'file2.yml')).toEqual(expectedOutputNested);
 });
